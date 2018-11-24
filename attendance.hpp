d528f58377beb0022b223b4facf64303b928edb2
Chaithanya Kumar VR <vrckumar@users.noreply.github.com>
2018-11-24 11:55:09 +0530

    Initial commit

---
+++ attendance.hpp
@@ -0,0 +1,45 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+CONTRACT attendance : public contract {
+
+  private:
+
+    enum login_status: int8_t  {
+      LOGGED_IN     = 0,
+      LOGGED_OUT   = 1
+    };
+
+    TABLE people_registry {
+      name    name;
+      string first_name;
+      string last_name;
+
+      uint64_t primary_key() const { return name.value;}
+    };
+
+    typedef multi_index<"people_registry"_n, people_registry> people_registry;
+
+    TABLE attendance_tracker {
+      name    name;
+      uint64_t        key = 1;
+      uint32_t        login_time = 1;
+      uint32_t        logout_time = 1;
+
+      auto primary_key() const { return key; }
+    };
+
+   typedef eosio::multi_index<"attendance_tracker"_n, attendance_tracker> attendance_tracker;
+
+    people_registry _registry;
+
+    attendance_tracker _tracker;
+
+    
+
+  public:
+
+    
+
+
+};
